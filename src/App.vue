e5ebe052b21019ec86a52de039d5071710fe8da3
thaysgama <thays.s.gama@gmail.com>
2022-03-19 12:51:24 -0300

    resolve conflitos no app

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,16 @@
 <template>
   <v-app>
-<<<<<<< HEAD
    <NavBar/>
-=======
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
       <router-view />
     </v-main>
->>>>>>> 1767a4c4743c14a09c180e179db7f600a6cc8e98
   </v-app>
 </template>
 
 <script>
-<<<<<<< HEAD
 
 import NavBar from './components/shared/NavBar.vue'
 
-=======
->>>>>>> 1767a4c4743c14a09c180e179db7f600a6cc8e98
 export default {
   name: "App",
 

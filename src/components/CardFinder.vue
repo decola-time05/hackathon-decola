18765ecb7acefc1c2c9fcf32965a04947b556706
Pedro Stanislau Tomacheski <tomacheski18@gmail.com>
2022-03-19 11:38:12 -0300

    Conexão com a API e criação do padrão dos cards

---
+++ src/components/CardFinder.vue
@@ -0,0 +1,30 @@
+<template>
+      <div>
+    <v-row class="justify-center">
+        <CardCreate v-for="card in blocosLista,i" :key="card.i" :card="card"/>
+    </v-row>
+  </div>
+</template>
+<script>
+import CardCreate from "./CardCreate.vue";
+export default {
+    components: {
+    CardCreate,
+  },
+  data() {
+    return {
+        blocosLista: [],
+    };
+  },
+  async created() {
+    const response = await fetch(
+      "https://it3-hbn-default-rtdb.firebaseio.com/carnaval.json"
+    );
+    const converted = await response.json();
+    this.blocosLista = converted;
+  },
+}
+</script>
+<style scoped>
+
+</style>

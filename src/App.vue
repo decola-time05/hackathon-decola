764449a48e4d80452d3e24ff198095c10956a331
thaysgama <thays.s.gama@gmail.com>
2022-03-19 13:02:47 -0300

    modifica footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-<<<<<<< HEAD
-    <NavBar />
-=======
    <NavBar/>
->>>>>>> e5ebe052b21019ec86a52de039d5071710fe8da3
     <v-main>
       <router-view/>
     </v-main>
@@ -12,14 +8,9 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-import NavBar from "./components/shared/NavBar.vue";
-
-=======
 
 import NavBar from './components/shared/NavBar.vue'
 
->>>>>>> e5ebe052b21019ec86a52de039d5071710fe8da3
 export default {
   name: "App",
 

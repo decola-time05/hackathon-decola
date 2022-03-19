edbb0deae1d918d19df23aa33415771686fc54b6
thaysgama <thays.s.gama@gmail.com>
2022-03-19 15:00:59 -0300

    adiciona footer

--- src/components/shared/AppFooter.vue
+++ src/components/shared/AppFooter.vue
@@ -21,14 +21,20 @@
         </v-btn>
       </v-card-text>
 
-      <v-card-text class="nwhite--text pt-0 d-flex">
-        <label class="d-flex align-center">Assine nossa newsletter:</label> 
-        <template>
-            <v-text-field class="mx-5" :rules="rules"></v-text-field>
-        </template>
-        <v-btn color="secondary">
-            Assinar
-        </v-btn>
+      <v-card-text class="nwhite--text pt-0">
+        <div class="newsletter d-flex align-center mx-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M7.5,15A1.5,1.5 0 0,0 6,16.5A1.5,1.5 0 0,0 7.5,18A1.5,1.5 0 0,0 9,16.5A1.5,1.5 0 0,0 7.5,15M6,10V12A6,6 0 0,1 12,18H14A8,8 0 0,0 6,10M6,6V8A10,10 0 0,1 16,18H18A12,12 0 0,0 6,6Z" /></svg>
+            <template>
+            <label>Assine nossa newsletter:</label> 
+                <v-text-field class="mx-3" :rules="rules"></v-text-field>
+            </template>
+        </div>
+        <div class="button-assinar mx-3">
+            <v-btn color="primary my-3">
+                        Assinar
+            </v-btn>
+        </div>
+        
       </v-card-text>
 
       <v-divider></v-divider>
@@ -50,6 +56,7 @@
         'mdi-linkedin',
         'mdi-instagram',
       ],
+      iconRss: 'mdi-rssbox',
       rules: [
         value => !!value || 'Required.',
         value => (value || '').length <= 20 || 'Max 20 characters',
@@ -66,5 +73,15 @@
 <style scoped>
 .footer-container{
     min-width: 100%;
+}
+.newsletter, .button-assinar{
+    max-width: 500px;
+}
+.button-assinar{
+    text-align: right;
+}
+svg{
+    fill: orange;
+    margin: 0px 5px;
 }
 </style>

cd7c8eb4dcb99d28dacc3cf241375a65f7564ce1
thaysgama <thays.s.gama@gmail.com>
2022-03-19 12:32:44 -0300

    adiciona card

--- src/components/CardCreate.vue
+++ src/components/CardCreate.vue
@@ -1,16 +1,60 @@
 <template>
-    <h2>teste</h2>
+    <v-card
+    class="mx-auto my-5"
+    max-width="344"
+  >
+    <v-img
+      :src=card.photo
+      height="350px"
+      width="350px"
+    ></v-img>
+
+    <v-card-title>
+      {{card.name}}
+    </v-card-title>
+
+    <v-card-subtitle>
+      {{card.address}}
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="amber darken-4"
+        text
+      >
+        ASSISTA AO VIVO
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          {{card.description}}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
 </template>
 
 <script>
 export default {
     name:'CardCreate',
     props:['card'],
-    data (){
-        return{
-            
-        }
-    }
+    data: () => ({
+      show: false,
+      
+    }),
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import CardView from '../views/CardView.vue'
 
 Vue.use(VueRouter)
 
@@ -14,10 +15,12 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: AboutView
+  },
+  {
+    path: '/carnaval',
+    name: 'CardView',
+    component: CardView
   }
 ]
 

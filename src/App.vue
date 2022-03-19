238e3c3028c900b7fcf5700012b92aab06567875
thaysgama <thays.s.gama@gmail.com>
2022-03-19 14:21:49 -0300

    inclui a newsletter

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
     <v-main>
       <router-view />
     </v-main>
+    <app-footer/>
   </v-app>
 </template>
 
 <script>
 import NavBar from "./components/shared/NavBar.vue";
+import AppFooter from '@/components/shared/AppFooter.vue'
 
 export default {
   name: "App",
 
   components: {
     NavBar,
+    AppFooter,
   },
 };
 </script>
--- src/components/shared/AppFooter.vue
+++ src/components/shared/AppFooter.vue
@@ -6,7 +6,7 @@
     <v-card
       flat
       tile
-      class="indigo lighten-1 white--text text-center"
+      class="footer-container grey darken-4 --text text-center"
     >
       <v-card-text>
         <v-btn
@@ -21,28 +21,50 @@
         </v-btn>
       </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      <v-card-text class="nwhite--text pt-0 d-flex">
+        <label class="d-flex align-center">Assine nossa newsletter:</label> 
+        <template>
+            <v-text-field class="mx-5" :rules="rules"></v-text-field>
+        </template>
+        <v-btn color="secondary">
+            Assinar
+        </v-btn>
       </v-card-text>
 
       <v-divider></v-divider>
 
       <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        {{ new Date().getFullYear() }} — <strong>copyright ©</strong>
       </v-card-text>
     </v-card>
   </v-footer>    
 </template>
 
 <script>
-
-export default {
+  export default {
     name: 'AppFooter',
-}
-
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+      rules: [
+        value => !!value || 'Required.',
+        value => (value || '').length <= 20 || 'Max 20 characters',
+        value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        },
+      ],
+    }),
+  }
 </script>
 
 
 <style scoped>
-
+.footer-container{
+    min-width: 100%;
+}
 </style>
